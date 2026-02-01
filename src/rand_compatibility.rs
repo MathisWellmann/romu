2648f8041c2d12a60f2130a727aa0b9be8506e5d
Nils Hasenbanck <nils@hasenbanck.de>
2026-02-01 16:26:04 +0100

    Fix compatibility with rand_core v0.10

--- src/rand_compatibility.rs
+++ src/rand_compatibility.rs
@@ -1,17 +1,22 @@
 //! Implements traits from the `rand` crate to provide compatibility with the broader ecosystem.
 
+use core::convert::Infallible;
+
 use crate::Rng;
 
-impl rand_core::Rng for Rng {
-    fn next_u32(&mut self) -> u32 {
-        self.u32()
+impl rand_core::TryRng for Rng {
+    type Error = Infallible;
+
+    fn try_next_u32(&mut self) -> Result<u32, Self::Error> {
+        Ok(self.u32())
     }
 
-    fn next_u64(&mut self) -> u64 {
-        self.u64()
+    fn try_next_u64(&mut self) -> Result<u64, Self::Error> {
+        Ok(self.u64())
     }
 
-    fn fill_bytes(&mut self, dst: &mut [u8]) {
-        Rng::fill_bytes(self, dst)
+    fn try_fill_bytes(&mut self, dst: &mut [u8]) -> Result<(), Self::Error> {
+        self.fill_bytes(dst);
+        Ok(())
     }
 }
